bf08abd03b762c8827cbec6e0a781f26e9e331f5
Ritav <ritav08092004@gmail.com>
2026-05-09 10:35:56 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,33 +1,34 @@
 module tb_PIPO();
-reg load_ti;
-reg clk_ti = 1'b0;
-reg [3:0]din_ti;
-reg rst_ti;
+reg       load_ti;
+reg       clk_ti = 1'b0;
+reg  [3:0]din_ti;
+reg       rst_ti;
 wire [3:0]dout_to;
 
 //instantiation
-PIPO DUT(.din_i(din_ti), 
+PIPO DUT(
+   .din_i(din_ti), 
    .load_i(load_ti), 
    .clk_i(clk_ti), 
    .rst_i(rst_ti), 
-   .dout_o(dout_to));
+   .dout_o(dout_to)
+);
 
 //clock
 initial begin
-   forever
-      #5 clk_ti = ~clk_ti;
+   forever #5 clk_ti = ~clk_ti;
 end
 
 //feeding
 initial begin
-rst_ti = 1'b1;
+rst_ti  = 1'b1;
 load_ti = 1'b0;
-din_ti = 4'b0000;
-#10 rst_ti = 1'b0;
+din_ti  = 4'b0000;
+#10 rst_ti  = 1'b0;
     load_ti = 1'b1;
-    din_ti = 4'b1010;
+    din_ti  = 4'b1010;
 #10 load_ti = 1'b0;
-    din_ti = 4'b1111;
+    din_ti  = 4'b1111;
 #20 load_ti = 1'b1;
 #10 load_ti = 1'b0;
 #10 $finish;
